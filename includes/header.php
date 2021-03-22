64dc7be4552fadcca860650322b2fd9ebf4809c8
awanz <hermawan9815@gmail.com>
2021-03-22 22:36:19 +0700

    update header logo on mobile

--- includes/header.php
+++ includes/header.php
@@ -29,9 +29,11 @@
 <header class="">
     <nav class="navbar navbar-expand-lg">
     <div class="container">
-        <a href="index.php">
-            <img src="assets/images/wizh2.png" alt="" style="width:150px">
-        </a>
+        <div class="mobilelogo">
+            <a href="index.php">
+                <img src="assets/images/wizh2.png" alt="" style="width:150px">
+            </a>
+        </div>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
